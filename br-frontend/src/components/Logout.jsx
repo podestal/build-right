bdc88ca2b93bccc691902297f1d5d0b7bc9fc4a0
podestal <luisrodriguezpodesta@gmail.com>
2024-01-26 07:27:28 -0500

    logout

--- br-frontend/src/components/Logout.jsx
+++ br-frontend/src/components/Logout.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import useAuth from '../hooks/useAuth'
 
 const Logout = () => {
+
+    const {setUser} = useAuth()
 
     const handleLogout = () => {
         localStorage.removeItem('access')
         localStorage.removeItem('refresh')
+        setUser()
     }
 
   return (
