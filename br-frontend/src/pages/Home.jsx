65fd27798c35f812835a9b6d290945909db22dca
podestal <luisrodriguezpodesta@gmail.com>
2024-01-30 09:24:38 -0500

    hero

--- br-frontend/src/pages/Home.jsx
+++ br-frontend/src/pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='hero'>
+      <dir className='hero-content'>
+        <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem corporis suscipit </h1>
+        <Link to={'/contact'} className='btn-primary '>Get a quote</Link>
+      </dir>
+    </div>
   )
 }
 
